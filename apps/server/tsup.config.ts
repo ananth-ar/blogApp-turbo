4805ab80ba0ee2b768c50178347589a977a5074f
ananth-ar <ananthravi841@gmail.com>
2024-08-16 14:14:24 +0530

    16.08 made in work

--- apps/server/tsup.config.ts
+++ apps/server/tsup.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig } from "tsup";
+import { defineConfig, type Options } from "tsup";
 
-export default defineConfig({
-  entry: ["./src/index.ts"],
-  noExternal: ["@example"], // Bundle any package starting with `@example` and their dependencies
-  splitting: false,
-  bundle: true,
-  outDir: "./dist",
+export default defineConfig((options: Options) => ({
+  entryPoints: ["src/index.ts"],
   clean: true,
-  env: { IS_SERVER_BUILD: "true" },
-  loader: { ".json": "copy" },
-  minify: true,
-  sourcemap: true,
-});
+  format: ["cjs"],
+  ...options,
+}));
