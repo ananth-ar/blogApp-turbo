5c43d8c9d64aa970258f9d4b589e1f812a8afa05
ananth-ar <ananthravi841@gmail.com>
2024-08-18 04:29:15 +0530

    finished turbo config

--- apps/blog/src/app/page.tsx
+++ apps/blog/src/app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@repo/ui/button";
 import Home from "../components/Home";
 
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
+      <Button
+        children={<span>hello from ui</span>}
+        appName="hi from ui"
+        className="bg-violet-600 text-white px-2 py-1 rounded-lg"
+      />
       <Home />
     </main>
   );
