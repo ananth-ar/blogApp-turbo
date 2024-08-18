9a99c478ed9a90b3d1ab21c5755ddc05793ee906
ananth-ar <ananthravi841@gmail.com>
2024-08-18 12:39:39 +0530

    18.08 h

--- apps/blog/src/app/page.tsx
+++ apps/blog/src/app/page.tsx
@@ -1,15 +1,61 @@
-import { Button } from "@repo/ui/button";
 import Home from "../components/Home";
+import { Button } from "../components/ui/button";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "../components/ui/command";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+
 
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      <Button
-        children={<span>hello from ui</span>}
-        appName="hi from ui"
-        className="bg-violet-600 text-white px-2 py-1 rounded-lg"
-      />
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
       <Home />
+      <Button variant="default">Button</Button>
+      <Command>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>Profile</CommandItem>
+            <CommandItem>Billing</CommandItem>
+            <CommandItem>Settings</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
     </main>
   );
 }
